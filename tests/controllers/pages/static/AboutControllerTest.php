0766a4e07d242f1ad7aa79ee6be535171893e125
Maxence <maxence.torchin@etu.univ-amu.fr>
2025-11-03 15:19:25 +0100

    Mise à jour de a propos vers about pour cohérence avec la langue du code

---
+++ tests/controllers/pages/static/AboutControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace controllers\pages\static;
+
+use modules\controllers\pages\static\AboutController;
+use PHPUnit\Framework\TestCase;
+
+class AboutControllerTest extends TestCase
+{
+
+}
